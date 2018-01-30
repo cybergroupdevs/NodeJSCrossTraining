4cde4b336fd8f0a28c1d5742937a87e900ffb0b9
Aashish Issar <aashish.Issar@cybergindia.local>
2018-01-30 17:41:06 +0530

    added delete user api

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const authentication = require('./../middleware/authentication/authentication.mi
 //TODO: how to require all routes from a folder such as: - require all routes from 
 //account folder
 router.use(require('./account'));
-router.use(authentication.jwtAuthentication.verify);
 
+router.use(authentication.jwtAuthentication.verify);
+router.use(require('./user'));
 module.exports = router;
