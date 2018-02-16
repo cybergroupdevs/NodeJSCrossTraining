eda5c75722e106c99b2c049a6071c4d7c798e248
vijay masiwal <vijay.masiwal@cygrp.com>
2018-02-16 18:29:00 +0530

    Added cors package.

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const mongoose = require('./dbconfig/mongodb.connect');
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8080;
 
 
 app.use(bodyParser.json());
+app.use(cors())
 app.use(routes);
 
 app.listen(port, () => {
